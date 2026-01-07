371d60286d328fd61fd06bb2984b343a9bbbd9fb
whattemup <fernandogarciarodriguez78@gmail.com>
2026-01-07 02:28:31 -0800

    create DATABASE SETUP

--- backend/db.py
+++ backend/db.py
@@ -1,3 +1,12 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
+DATABASE_URL = "sqlite:///./easyfinder.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
