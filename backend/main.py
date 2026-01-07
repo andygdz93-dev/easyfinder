6fc63f9fcb8ceed6cae3bd8f0cdcd2dd3ccd36b4
whattemup <fernandogarciarodriguez78@gmail.com>
2026-01-07 00:22:27 -0800

    fixed errors

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
-from routes.inventory import router as inventory_router
+from backend.routes.inventory import router as inventory_router
 import random
 
 app = FastAPI(title="EasyFinder AI Backend")
