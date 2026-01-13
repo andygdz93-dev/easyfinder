9e00f99b2e56d2883f6a16c6f0967c8232aa443d
whattemup <fernandogarciarodriguez78@gmail.com>
2026-01-12 18:30:53 -0800

    apply fix

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {colors: {
+  brand: {
+    blue: "#0B1C2D",
+    orange: "#F97316",
+  },
+},
+},
   },
   plugins: [],
 };
